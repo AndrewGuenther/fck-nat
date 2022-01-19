d99af8f9dd36fb100d97d344c44db606bb0b0459
Andrew Guenther <guenther.andrew.j@gmail.com>
2022-01-19 00:04:31 -0800

    Dynamically select interface name

--- service/fck-nat.sh
+++ service/fck-nat.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# Grab the interface with the default route
+interface=$(ip route | grep default | cut -d ' ' -f 5)
+
 sysctl -q -w net.ipv4.ip_forward=1
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE -m comment --comment "NAT routing rule installed by fck-nat"
+iptables -t nat -A POSTROUTING -o "$interface" -j MASQUERADE -m comment --comment "NAT routing rule installed by fck-nat"
