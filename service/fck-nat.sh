364c50dc134ae787dd630529fcee8c78c3f75499
Ra'Jiska <dodo.lasticot@gmail.com>
2023-11-19 22:07:10 +0800

    Differentiation between public & private interface

--- service/fck-nat.sh
+++ service/fck-nat.sh
@@ -11,8 +11,6 @@ aws_region="$(/opt/aws/bin/ec2-metadata -z | cut -f2 -d' ' | sed 's/.$//')"
 eth0_mac="$(cat /sys/class/net/eth0/address)"
 token="$(curl -X PUT -H 'X-aws-ec2-metadata-token-ttl-seconds: 300' http://169.254.169.254/latest/api/token)"
 eth0_eni_id="$(curl -H "X-aws-ec2-metadata-token: $token" http://169.254.169.254/latest/meta-data/network/interfaces/macs/$eth0_mac/interface-id)"
-eth0_ipv4="$(curl -s -H "X-aws-ec2-metadata-token: $token" http://169.254.169.254/latest/meta-data/network/interfaces/macs/$eth0_mac/local-ipv4s | head -n 1)"
-eth0_ipv6="$(curl -s -H "X-aws-ec2-metadata-token: $token" http://169.254.169.254/latest/meta-data/network/interfaces/macs/$eth0_mac/ipv6s | head -n 1)"
 
 if test -n "$eip_id"; then
     echo "Found eip_id configuration, associating $eip_id..."
@@ -49,13 +47,16 @@ if test -n "$eni_id"; then
         sleep 1
     done
 
-    nat_interface="eth0"
+    nat_public_interface="eth0"
+    nat_private_interface="eth1"
 elif test -n "$interface"; then
     echo "Found interface configuration, using $interface"
-    nat_interface=$interface
+    nat_public_interface=$interface
+    nat_private_interface=$nat_public_interface
 else
-    nat_interface=$(ip route | grep default | cut -d ' ' -f 5)
-    echo "No eni_id or interface configuration found, using default interface $nat_interface"
+    nat_public_interface=$(ip route | grep default | cut -d ' ' -f 5)
+    nat_private_interface=$nat_public_interface
+    echo "No eni_id or interface configuration found, using default interface $nat_public_interface"
 fi
 
 echo "Enabling IPv4 forwarding..."
@@ -69,13 +70,17 @@ done
 echo "Flushing IPv4 NAT table..."
 iptables -t nat -F
 
-echo "Adding IPv4 NAT rule..."
-iptables -t nat -A POSTROUTING -o "$nat_interface" -j MASQUERADE -m comment --comment "NAT routing rule installed by fck-nat"
+echo "Adding IPv4 NAT rules..."
+iptables -t nat -A POSTROUTING -o "$nat_public_interface" -j MASQUERADE -m comment --comment "NAT routing rule installed by fck-nat"
 
 if test -n "$nat64_enabled"; then
     echo "Found nat64_enabled configuration, setting up NAT64 via TAYGA..."
+    nat_public_interface_ipv4="$(curl -s -H "X-aws-ec2-metadata-token: $token" http://169.254.169.254/latest/meta-data/network/interfaces/macs/$eth0_mac/local-ipv4s | head -n 1)"
+    nat_public_interface_ipv6="$(curl -s -H "X-aws-ec2-metadata-token: $token" http://169.254.169.254/latest/meta-data/network/interfaces/macs/$eth0_mac/ipv6s | head -n 1)"
+
     pkill tayga
     /usr/local/sbin/tayga --rmtun
+
     cat <<EOF > /usr/local/etc/tayga.conf
 tun-device nat64
 ipv4-addr ${nat64_ipv4_addr:-192.168.255.1}
@@ -84,17 +89,19 @@ prefix 64:ff9b::/96
 dynamic-pool ${nat64_ipv4_dynamic_pool:-192.168.0.0/16}
 data-dir /var/db/tayga
 EOF
+
+    echo "Creating nat64 interface..."
     /usr/local/sbin/tayga --mktun
     ip link set nat64 up
-    ip addr add "$eth0_ipv4" dev nat64
-    ip addr add "$eth0_ipv6" dev nat64
+    ip addr add "$nat_public_interface_ipv4" dev nat64
+    ip addr add "$nat_public_interface_ipv6" dev nat64
     ip route add "${nat64_ipv4_dynamic_pool:-192.168.0.0/16}" dev nat64
     ip route add 64:ff9b::/96 dev nat64
     /usr/local/sbin/tayga
 
     echo "Enabling IPv6 forwarding..."
-    sysctl -q -w net.ipv6.conf.eth0.accept_ra=2
-    sysctl -q -w net.ipv6.conf.eth1.accept_ra=2
+    sysctl -q -w net.ipv6.conf."$nat_public_interface".accept_ra=2
+    sysctl -q -w net.ipv6.conf."$nat_private_interface".accept_ra=2
     sysctl -q -w net.ipv6.conf.all.forwarding=1
 fi
 
